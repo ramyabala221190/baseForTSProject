433b4449e7869ad9faf5d4a9147df7b62ca1c639
Ramya <ramya.bala221190@gmail.com>
2025-02-08 16:15:31 +0530

    Added changes for autoprefixer

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
-This is a simple base project testing your TS code in the browser.
+For AutoPrefixer,
 
-We are using Webpack to bundle the code
+Install the below dev dependencies
+npm install --save-dev postcss postcss-loader postcss-preset-env
 
-1. 
+We will create a webpack.config.2.js in the root of the project which is the exact replica of webpack.config.1.js.
+
+We will make a small change in the module.rules key. 
+Update FROM
+{
+test:/\.scss$/,
+exclude:/node_modules/,
+use:[miniCssExtractPlugin.loader,"css-loader","sass-loader"], //executes from right to left
+},
+
+TO
+
+{
+test:/\.scss$/,
+exclude:/node_modules/,
+use:[miniCssExtractPlugin.loader,"css-loader","sass-loader",
+{
+loader:"postcss-loader",
+options:{
+postcssOptions:{
+plugins:[
+["postcss-preset-env",{}]
+]
+}
+}}], //executes from right to left
+}
+
+
+Add commands in package.json
+"build-2":"webpack --env production --config webpack.config.2.js",
+"start-2": "webpack serve --config webpack.config.2.js"
---
+++ webpack.config.2.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const htmlWebpackPlugin=require('html-webpack-plugin');
+const miniCssExtractPlugin=require('mini-css-extract-plugin');
+
+let webpackConfig= (env)=>{
+  console.log("environment variables logged",env)
+  return {
+    entry:"./src/index", //entry point file
+    output:{
+      filename: "output.js", //default filename is main.js,
+      path: path.resolve(__dirname,"dist"),
+      clean:true // empty the dist folder before every new build
+    },
+    mode: env.production ? "production" : "development",
+    devtool: env.production ? "source-map":"inline-source-map",
+    devServer:{
+      static:'./dist'
+    },
+    resolve:{
+      extensions:['.ts','.js']
+    },
+    module:{
+      rules:[
+        {
+          test:/\.ts$/,
+          exclude:/node_modules/,
+          use:'ts-loader'
+        },
+        {
+          test:/\.scss$/,
+          exclude:/node_modules/,
+          use:[miniCssExtractPlugin.loader,"css-loader","sass-loader",
+        {
+            loader:"postcss-loader",
+            options:{
+                postcssOptions:{
+                    plugins:[
+                        ["postcss-preset-env",{}]
+                    ]
+                }
+            }
+        }], //executes from right to left
+        },
+       
+      ]
+    },
+    plugins:[
+      new htmlWebpackPlugin({
+        template:'./index.html'
+      }),
+      new miniCssExtractPlugin({filename:'output.css'})
+    ]
+  }
+}
+
+module.exports = webpackConfig;
